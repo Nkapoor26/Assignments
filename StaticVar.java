97d6096ea8e68b6865f3e39d0a7ed17c24cb03a4
Neha Kapoor <k.neha261193@gmail.com>
2017-04-25 20:33:01 +0530

    Add files via upload

---
+++ StaticVar.java
@@ -0,0 +1,14 @@
+package prg15;
+
+public class StaticVar {
+int Id;
+String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StaticVar s=new StaticVar();
+System.out.println(s.Id);
+System.out.println(s.name);
+
+	}
+
+}
