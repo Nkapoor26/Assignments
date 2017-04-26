48c4b716629f2c3f1d8ee911423a1acaab372cb4
Neha Kapoor <k.neha261193@gmail.com>
2017-04-26 15:18:10 +0530

    Add files via upload

---
+++ StringMut.java
@@ -0,0 +1,15 @@
+package prg20;
+
+public class StringMut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("Keyboard");
+
+		{
+			sb.append("soft");
+			System.out.println(sb);
+		}
+	}
+
+}
